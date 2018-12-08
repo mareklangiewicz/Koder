781ba21291fa6ae70c3a1e2e9e6be30b23ff4a31
Marek Langiewicz <marek.langiewicz@gmail.com>
2018-12-08 14:39:50 +0100

    Add some common tests

---
+++ src/commonTest/kotlin/sample/KoderTests.kt
@@ -0,0 +1,30 @@
+package sample
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class KoderTests {
+
+    @Test
+    fun testEncodeToString() {
+        checkEncodeToString("Kotlin is awesome", "S290bGluIGlzIGF3ZXNvbWU=")
+    }
+
+    @Test
+    fun testPaddedStrings() {
+        checkEncodeToString("", "")
+        checkEncodeToString("1", "MQ==")
+        checkEncodeToString("22", "MjI=")
+        checkEncodeToString("333", "MzMz")
+        checkEncodeToString("4444", "NDQ0NA==")
+    }
+
+    private fun checkEncodeToString(input: String, expectedOutput: String) {
+        assertEquals(expectedOutput, KoderFactory.provideKoder().encodeToString("xxx", input.asciiToByteArray()))
+    }
+
+    private fun String.asciiToByteArray() = ByteArray(length) {
+        get(it).toByte()
+    }
+}
