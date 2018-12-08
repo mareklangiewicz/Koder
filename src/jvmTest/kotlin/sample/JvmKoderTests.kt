8cf4fc47b97cf98638f815ce09301cbe043055d5
Marek Langiewicz <marek.langiewicz@gmail.com>
2018-12-08 14:44:32 +0100

    Add jvm specific test

---
+++ src/jvmTest/kotlin/sample/JvmKoderTests.kt
@@ -0,0 +1,14 @@
+package sample
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class JvmKoderTests {
+    @Test
+    fun testNonAsciiString() {
+        val utf8String = "Gödel"
+        val actual = KoderFactory.provideKoder().encodeToString("xxx", utf8String.toByteArray())
+        assertEquals("R8O2ZGVs", actual)
+    }
+}
